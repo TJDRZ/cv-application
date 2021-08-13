f74596e439bfd6f407c67e3b35546b36b0360885
TJ <tjdrzewiecki@gmail.com>
2021-08-13 17:08:53 -0400

    Save/buttons updated: @media print added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import Header from "./components/Header";
 import Container from "./components/Container";
 import School from "./components/School";
 import Work from "./components/Work";
-import save from "./save";
 import "./styles/style.css";
 
 function App() {
@@ -21,7 +20,7 @@ function App() {
         type="Work"
         title={true}
       />
-      <button onClick={() => save()}>Save</button>
+      <button onClick={() => window.print()}>Save</button>
     </div>
   );
 }
--- src/save.js
+++
@@ -1,8 +0,0 @@
-function save() {
-  const buttons = document.querySelectorAll("button");
-  buttons.forEach((button) => (button.style.display = "none"));
-  window.print();
-  buttons.forEach((button) => (button.style.display = "inline"));
-}
-
-export default save;
